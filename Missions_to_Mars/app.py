b74fe9c5f49c38e3706c1abe2691c4771dd2c4d6
Autzen <kristyskisfast@gmail.com>
2020-09-20 19:18:47 -0700

    changes

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,43 @@
+# Step 2 - MongoDB and Flask Application
+# look in activity 12 03 01-07
+
+# import necessary libraries
+# and pymongo library, which connects Flask app to our Mongo database.
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+import scrape_mars
+
+# create instance of Flask app
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection, from activity 12-03-09
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+@app.route("/")
+def index():
+    mars_info = mongo.db.mars_info.find_one()
+    
+    return render_template("index.html", mars_info=mars_info)
+
+
+@app.route("/scrape")
+def scrape():
+    mars_info = mongo.db.mars_info
+    # ?
+    mars_info = mission_to_mars.scrape_mars_news()
+    # ?
+    mars_info.update({}, mars_data, upsert=True)
+    
+    return redirect(url_for("index"), code=302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+# {% if (variable) %}
+
+# .find_one(),
